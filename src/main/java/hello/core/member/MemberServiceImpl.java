36debe83fd14485a8b311b9e12c0a08b938264ff
sol <sadlthf@icloud.com>
2022-08-18 13:55:04 +0900

    #1 - MemberService 클래스명 오타 수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,8 @@
 package hello.core.member;
 
-public class memberServiceImpl implements memberService {
+public class MemberServiceImpl implements MemberService {
 
+    // MemberServiceImpl은 추상화(MemberRepostiory) 에 의존하는 동시에, 구체화(MemoryMemberRepository) 에도 의존한다. -> DIP 를 위반하는 중
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
