445636b26fcc49b1e49115a079e5c499b397977d
sol <sadlthf@icloud.com>
2022-08-20 23:29:51 +0900

    #3 - 관심사의 분리[OCP, DIP 지키도록 변경]
    * AppConfig를 이용한 생성자 주입으로 `dependency injection(의존관계주입)` 을 한다.

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+// 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 한다.
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+}
