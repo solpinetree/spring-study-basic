ddda17e6b04463afeb47a51f7152df6cc5d577f4
sol <sadlthf@icloud.com>
2022-08-21 01:05:33 +0900

    #3 - 순수 자바 코드로 했던 DI를 스프링으로 전환
    * `ApplicationContext` 를 `스프링 컨테이너` 라고 한다.
    * 이전 코드에서는 순수 자바 코드로 `AppConfig` 를 사용해서 직접 객체를 생성하고 `DI`를 했지만,  이번 코드에서는 `스프링 컨테이너`를 이용해서 `DI`를 한다.
    * 스프링 빈은 `applicationContext.getBean()` 을 이용해서 찾을 수 있다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,30 +1,36 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 // 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 한다.
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemoryMemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
+    @Bean // spring container에 등록이 된다.
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
