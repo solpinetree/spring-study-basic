c9bcbfca32d2a9f13434b210ccc288bde513bb4c
sol <sadlthf@icloud.com>
2022-08-25 10:01:10 +0900

    #9 - 컴포넌트 스캔과 의존관계 자동 주입 + 테스트 코드 작성
    * 컴포넌트 스캔을 사용하려면 먼저 `@ComponentScan` 을 설정 정보에 붙여준다.
    * 기존의 AppConfig 와 다르게 @Bean으로 등록한 클래스가 하나도 없음
    * 컴포넌트 스캔은 이름 그대로 `@Component` 어노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록
    * `@Autowired`는 의존관계를 자동으로 주입해줌
    
    컴포넌트 스캔을 사용하면 `@Configuration`이 붙은 설정 정보도 자동으로 등록되기 때문에 다른 설정 정보들도 함께 등록되고 실행된다. 그래서 `excludeFilters`을 이용해서 컴포넌트 스캔 대상에서 제외. 하지만, 이는 지금 예제를 수월하게 진행하기 위함이고, **보통은 설정 정보를 컴포넌트 스캔 대상에서 제외하지는 않는다.**

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -11,6 +14,7 @@ public class OrderServiceImpl implements OrderService {
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // OCP와 DIP를 위반
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 코드를 변경
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
