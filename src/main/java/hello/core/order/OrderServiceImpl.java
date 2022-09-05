b029ce993af764447226f3573c991514d11c324f
sol <sadlthf@icloud.com>
2022-09-05 22:53:19 +0900

    #11 - OrderServiceImpl class에 lombok 도입
    * `@RequiredConstructor` 기능을 사용하면 `final`이 붙은 필드를 모아서 생성자를 자동으로 만들어줌.
    * 최근에는 생성자를 딱 1개 두면서, `@Autowired` 를 생략하는 방법을 주로 사용. 
    -> 두 개를 합치면 깔끔한 코드로 기능을 사용할 수 있다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,22 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final variable을 생성자 파라미터로 만들어줌
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // OCP와 DIP를 위반
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 코드를 변경
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // 생성자가 하나이면 @Autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
